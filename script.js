2d3c629e3fb209ae0d385de8ebdbf3ba3bf451e1
Marc Vivas <vivasmarc001@gmail.com>
2023-08-21 11:38:14 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+let currentSection = document.querySelector("#sec1");
+const aboutMeCard = document.querySelector("#about-me");
+const techStackCard = document.querySelector("#tech-stack");
+const projectsCard = document.querySelector("#projects");
+const getInTouchCard = document.querySelector("#get-in-touch");
+
+
+function home(e){
+    currentSection.style.display = "none";
+    currentSection = document.querySelector("#sec1");
+    currentSection.style.display = "flex";
+}
+
+function showAboutMe(e){
+    currentSection.style.display = "none";
+    currentSection = document.querySelector("#about");
+    currentSection.style.display = "flex";
+}
+
+function showTechStack(e){
+    currentSection.style.display = "none";
+    currentSection = document.querySelector("#tech-stack-sec");
+    currentSection.style.display = "flex";
+}
+
+function showProjects(e){
+    currentSection.style.display = "none";
+
+}
+
+function showGetInTouch(e){
+    currentSection.style.display = "none";
+
+}
+
+aboutMeCard.addEventListener("click", showAboutMe);
+techStackCard.addEventListener("click", showTechStack);
+projectsCard.addEventListener("click", showProjects);
+getInTouchCard.addEventListener("click", showGetInTouch);
